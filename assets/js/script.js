06e6e9458f81a5a9ed2ffe9f65828958a34b169e
Charlie Lambino-Worthington <charlie.m.k.worthington@gmail.com>
2024-05-17 13:12:00 +0000

    fix bug with addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons){
-        button.addEventListener("click", function(){
-            if (this.getAttribute("data-type") === "submit"){
-                alert("You clicked submit!");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -16,31 +16,26 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     runGame("addition");
+
 });
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-
 function runGame(gameType) {
 
-    // Creates two random numbers between 1 and 25  
-let num1 = math.floor(math.random() * 25 + 1);
-let num2 = math.floor(math.random() * 25  + 1);
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-    if (gameType === addition) {
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else if (gameType === subtract) {
-        displaySubtractQuestion(num1, num2);
-    } else if (gameType === multiply) {
-        displayMultiplyQuestion(num1, num2);
-    } else if (gameType === division) {
-        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
 
 function checkAnswer() {
@@ -60,19 +55,17 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "+";
-}
 
-function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
 }
 
-function displayMultiplyQuestion(operand1, operand2) {
-    
+function displaySubtractQuestion() {
+
 }
 
-function displayDivisionQuestion(operand1, operand2) {
+function displayMultiplyQuestion() {
     
 }
